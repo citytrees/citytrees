7bdf061c8e75bc1415e8367f12fad36ace7102ad
Maksim Nagovitsin <m.nagovitsin@tinkoff.ru>
2022-09-17 12:21:53 +0500

    fix CI

--- src/test/java/io/citytrees/AbstractTest.kt
+++ src/test/java/io/citytrees/AbstractTest.kt
@@ -1,12 +1,14 @@
 package io.citytrees
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.Tag
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 
+@Tag("integration")
 @SpringBootTest
 @ActiveProfiles("dev")
 @AutoConfigureMockMvc
